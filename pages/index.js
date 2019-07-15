156e31e21d75527bdd1f1f9ab299612bb1b8a05d
Kim Jae Hyune <wogus4041@naver.com>
2019-07-15 21:17:03 +0900

    admin 수정

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
-import Layout from '../components/layout'
+import React, { Component } from 'react'
+import content from '../content/articles.md';
 
-export default () => (
-  <Layout>
-    <section className="section">
-      <div className="container">
-        <h1>인덱스 페이지</h1>
-      </div>
-    </section>
-  </Layout>
-);
+export default class Home extends Component {
+  render() {
+    let { html , attributes:{ title, cats } } = content;
+    return (
+      <article>
+          <h1>{title}</h1>
+          <div dangerouslySetInnerHTML={{ __html: html }}/>
+          <ul>
+              { cats.map((cat, k) => (
+                  <li key={k}>
+                    <h2>{cat.name}</h2>
+                    <p>{cat.description}</p>
+                  </li>
+              ))}
+          </ul>
+      </article>
+    )
+  }
+}
